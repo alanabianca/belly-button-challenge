f7c46326f78871c318016bbd69fe421a29b47b21
alanabianca <141048321+alanabianca@users.noreply.github.com>
2024-01-11 12:52:39 -0600

    Bar Graph initialized

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,65 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-const url2 = "https://api.spacexdata.com/v4/launchpads"
 
 //Promise Pending
 const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+dataPromise.then(function(data){
+    console.log("Data Promise: Resolved: ", data);
 
-//Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-    console.log(data);
-
-    //Populate the dropdowm menu using  the names array
+// Populate the dropdown menu using the names array
     var dropdown = d3.select("#dropdown");
     
     data.names.forEach((name) => {
         dropdown.append("option").text(name).attr("value", name);
     });
+
+//Grab the correct data to send to the bar graph
+    var nameSelect = "940";
+    var currentSample = data.samples.find(sample => sample.id === nameSelect);
+
+    //This will select the largest 10 numbers for the values
+    var otuIds = currentSample.otu_ids;
+    var SlicedValues = otuIds.slice(0,10);
+    var yvalues = SlicedValues.map(function(number){
+        return "OTU " + number.toString();
+    });
+
+    var Samples = currentSample.sample_values;
+    var xvalues = Samples.slice(0,10);
+    init(xvalues, yvalues);
 });
+
+
+// *Make the bar chart*
+function init(xvalues, yvalues){
+
+let books = ["One", "Two", "Three"]
+let timesRead = [100, 50, 25]
+
+let trace1 = {
+  x: xvalues,
+  y: yvalues,
+  type: 'bar',
+  orientation: 'h'
+};
+
+let data = [trace1];
+
+let layout = {
+  title: "Alana's Bar Chart"
+};
+
+Plotly.newPlot("barplot", data, layout);
+}
+
+//This all happens when you click something:
+
+// Call updatePlotly() when a change takes place to the DOM
+d3.selectAll("#dropdown").on("change", updatePlotly);
+
+function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#dropdown");
+    // Assign the value of the dropdown menu option to a variable
+    let dataset = dropdownMenu.property("value");
+    console.log(dataset);
+}
